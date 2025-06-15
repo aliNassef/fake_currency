32a66085d6c7a8661a1ab0eb2ce5af2258377292
aliNassef <anassef798@gmail.com>
2025-06-15 04:31:04 +0300

    ..

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'core/helper/custom_bloc_observer.dart';
