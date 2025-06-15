9124a78d09cc21a93ecfb6b49b73ac309643a46e
aliNassef <anassef798@gmail.com>
2025-06-15 04:17:11 +0300

    done

--- lib/fake_currency_app.dart
+++ lib/fake_currency_app.dart
@@ -1,8 +1,11 @@
 import 'package:device_preview/device_preview.dart';
+import 'package:fake_currency/core/di/injection.dart';
 import 'package:fake_currency/core/utils/app_colors.dart';
 import 'package:fake_currency/features/auth/presentation/view/signup_view.dart';
+import 'package:fake_currency/features/main/presentation/views/main_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class FakeCurrency extends StatelessWidget {
   const FakeCurrency({super.key});
@@ -22,7 +25,7 @@ class FakeCurrency extends StatelessWidget {
               primaryColor: AppColors.primaryColor,
             ),
             debugShowCheckedModeBanner: false,
-            home: const SignupView(),
+            home: injector<SharedPreferences>().getString('token') != null ? const MainView() : const SignupView(),
           ),
     );
   }
